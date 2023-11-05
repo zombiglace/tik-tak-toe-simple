259802f04b65bda62ce7f8b2ba7bea85ba8d93f4
zombiglace <101476751+zombiglace@users.noreply.github.com>
2023-11-05 14:32:40 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #f2f2f2;
+        text-align: center;
+      }
+
+      h1 {
+        color: #333;
+      }
+
+      #board {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-gap: 5px;
+        width: 300px;
+        margin: 0 auto 10px;
+        background-color: #fff;
+        padding: 10px;
+        border-radius: 5px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+      }
+
+      .cell {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 24px;
+        background-color: #eee;
+        cursor: pointer;
+        border-radius: 5px;
+        transition: background-color 0.3s ease;
+      }
+
+      .cell:hover {
+        background-color: #ddd;
+      }
+
+      button {
+        font-size: 18px;
+        padding: 10px 20px;
+        cursor: pointer;
+        background-color: #333;
+        color: #fff;
+        border: none;
+        border-radius: 5px;
+        transition: background-color 0.3s ease;
+      }
+
+      button:hover {
+        background-color: #555;
+      }
+    </style>
+    <h1>Tic Tac Toe</h1>
+    <div id="board">
+      <div class="cell" onclick="makeMove(0)">1</div>
+      <div class="cell" onclick="makeMove(1)">2</div>
+      <div class="cell" onclick="makeMove(2)">3</div>
+      <div class="cell" onclick="makeMove(3)">4</div>
+      <div class="cell" onclick="makeMove(4)">5</div>
+      <div class="cell" onclick="makeMove(5)">6</div>
+      <div class="cell" onclick="makeMove(6)">7</div>
+      <div class="cell" onclick="makeMove(7)">8</div>
+      <div class="cell" onclick="makeMove(8)">9</div>
+    </div>
+    <button onclick="resetGame()">Réinitialiser</button>
+  </body>
+  <script>
+    let board = ["", "", "", "", "", "", "", "", ""];
+    let currentPlayer = "X";
+
+    function makeMove(index) {
+      if (board[index] === "") {
+        board[index] = currentPlayer;
+        document.getElementsByClassName("cell")[index].innerText =
+          currentPlayer;
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+      }
+      checkWinner();
+    }
+
+    function checkWinner() {
+      const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8], // lignes
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8], // colonnes
+        [0, 4, 8],
+        [2, 4, 6], // diagonales
+      ];
+
+      for (let condition of winConditions) {
+        const [a, b, c] = condition;
+        if (board[a] !== "" && board[a] === board[b] && board[a] === board[c]) {
+          alert(`Le joueur ${board[a]} a gagné !`);
+          resetGame();
+          return;
+        }
+      }
+
+      if (!board.includes("")) {
+        alert("Match nul !");
+        resetGame();
+      }
+    }
+
+    function resetGame() {
+      location.reload();
+    }
+  </script>
+</html>
